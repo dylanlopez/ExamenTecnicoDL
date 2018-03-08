26d0609e45f93fcef7122a382461bf842335728c
Dylan Lopez <dylanalejandro.lopezenciso@gmail.com>
2018-03-07 21:41:28 -0500

    Add files via upload

---
+++ AppParte03/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppParte03.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
